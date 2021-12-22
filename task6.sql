0b745540d4807add56aa809e3a60e1116735c705
andrros3030 <66036268+andrros3030@users.noreply.github.com>
2021-12-22 23:23:42 +0300

    tsk6 comments

--- task6.sql
+++ task6.sql
@@ -1,9 +1,9 @@
 select
   rec.oper_date date
-, sum(rec.sum) operations_sum
-, clnt.name client_name
+, sum(rec.sum) operations_sum -- сумма операций за дату операций
+, clnt.name client_name -- клиент
 from records rec
 join accounts acc on rec.acc_ref = acc.id
 join CLIENTS clnt on acc.client_ref = clnt.ID
-where rec.oper_date between datetime('now', '-1 month') and datetime('now')
-GROUP by rec.oper_date
+where rec.oper_date between datetime('now', '-1 month') and datetime('now') -- все операции за последний месяц
+GROUP by rec.oper_date -- в разрезе даты
