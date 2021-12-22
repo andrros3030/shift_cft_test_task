bda8c493412326dd9eaa986fad938b10e8c07d1e
andrros3030 <66036268+andrros3030@users.noreply.github.com>
2021-12-22 20:02:28 +0300

    task 6

--- dml2.sql
+++ dml2.sql
@@ -5,4 +5,10 @@ insert into PRODUCTS(ID, PRODUCT_TYPE_ID, NAME, CLIENT_REF, OPEN_DATE, CLOSE_DAT
 insert into ACCOUNTS(ID, NAME, SALDO, CLIENT_REF, OPEN_DATE, CLOSE_DATE, PRODUCT_REF, ACC_NUM) values (222, 'Депозитный счет для Петрова С.И.', 6000, 3,'2017-08-01', null, 333, '333222333');
 
 insert into RECORDS(ID, DT, SUM, ACC_REF, OPER_DATE) values (111, 1, 5000, 1, '2021-11-28');
-insert into RECORDS(ID, DT, SUM, ACC_REF, OPER_DATE) values (222, 1, 5000, 222, '2021-12-10')
+insert into RECORDS(ID, DT, SUM, ACC_REF, OPER_DATE) values (222, 1, 5000, 222, '2021-12-10');
+
+insert into RECORDS(ID, DT, SUM, ACC_REF, OPER_DATE) values (123, 1, 2000, 2, '2021-11-28');
+insert into RECORDS(ID, DT, SUM, ACC_REF, OPER_DATE) values (124, 1, 500, 222, '2021-12-10');
+
+insert into RECORDS(ID, DT, SUM, ACC_REF, OPER_DATE) values (125, 1, 3000, 1, '2021-11-30');
+insert into RECORDS(ID, DT, SUM, ACC_REF, OPER_DATE) values (126, 1, 200, 222, '2021-12-11');
--- task6.sql
+++ task6.sql
@@ -1,13 +1,9 @@
-SELECT
-  clnt.ID id
-, clnt.NAME name
-, (SELECT
-   SUM(_rec.sum)
-   from ACCOUNTS _acc
-   join RECORDS _rec on _acc.id = _rec.acc_ref
-   where clnt.id = _acc.client_ref
-   and _rec.oper_date BETWEEN datetime('now', '-1 month')  and datetime('now')
-  ) operations_sum
---, sum(rec.sum)
-FROM CLIENTS clnt
-where operations_sum not null
+select
+  rec.oper_date date
+, sum(rec.sum) operations_sum
+, clnt.name client_name
+from records rec
+join accounts acc on rec.acc_ref = acc.id
+join CLIENTS clnt on acc.client_ref = clnt.ID
+where rec.oper_date between datetime('now', '-1 month') and datetime('now')
+GROUP by rec.oper_date
